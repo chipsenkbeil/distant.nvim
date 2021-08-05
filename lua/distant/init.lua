0c94d8d0360e3586d241d46ba0be08a3b1d50127
Chip Senkbeil <chip@senkbeil.org>
2021-08-05 11:23:01 -0500

    Initial commit

--- lua/distant/init.lua
+++ lua/distant/init.lua
@@ -1,6 +1,7 @@
 return {
     action = require('distant.action');
     fn = require('distant.fn');
+    lsp = require('distant.lsp');
     session = require('distant.session');
     setup = require('distant.setup');
 }
