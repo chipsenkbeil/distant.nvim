71d1ca8dfdab24c0fb4c7d91abaac3896dc2e21f
Chip Senkbeil <chipsenkbeil@fb.com>
2021-09-09 17:54:27 -0500

    Add executed copy test

---
+++ spec/e2e/fn/copy_spec.lua
@@ -0,0 +1,33 @@
+local fn = require('distant.fn')
+local Driver = require('spec.e2e.driver')
+
+describe('fn', function()
+    local driver, dir, file
+
+    before_each(function()
+        driver = Driver:setup()
+
+        -- Create a test dir and file on the remote machine
+        dir = driver:new_dir_fixture()
+        file = dir.file()
+
+        -- Populate test file with some content
+        file.write('some file content')
+    end)
+
+    after_each(function()
+        driver:teardown()
+    end)
+
+    describe('copy', function()
+        it('should copy a src file to a destination', function()
+            local dst = dir.file()
+
+            local err, res = fn.copy(file.path(), dst.path())
+            assert(not err, err)
+            assert.is.truthy(res)
+
+            dst.assert.same({'some file content'})
+        end)
+    end)
+end)
